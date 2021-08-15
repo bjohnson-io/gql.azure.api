2d95b993ef49a7550765179b95e6b2c8fc383d95
b.johnson <b.johnson@fan-gene.com>
2021-08-15 19:02:16 -0400

    update port for prod env

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-if (process.env.NODE_ENV !== 'production') {
+const isProductionEnvironment = process.env.NODE_ENV !== 'production';
+if (isProductionEnvironment) {
   require('dotenv').config();
 }
 import { NestFactory } from '@nestjs/core';
@@ -6,6 +7,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(isProductionEnvironment ? 80 : 3000);
 }
 bootstrap();
